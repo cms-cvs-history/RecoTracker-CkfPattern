5763a61124a902c5696ba5fba6252fab95f03431
cmsbuild <>
2006-05-30 00:29:29 +0000

    This commit was manufactured by cvs2git to create tag 'RecoTracker- CkfPattern-V00-01-00'.
    Sprout from master 2006-05-30 00:29:27 UTC Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch> 'brought parameter-sets to CMSSW_0_6_1 state'
    Cherrypick from master 2006-04-26 20:48:30 UTC Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch> 'fitter and smoother removed':
        BuildFile
        interface/CombinatorialTrajectoryBuilder.h
        interface/KFTrackCandidateMaker.h
        src/CombinatorialTrajectoryBuilder.cc
        src/KFTrackCandidateMaker.cc
        src/SealModules.cc
        test/DigisToRecHits.cfg
        test/GenToSimHits.cfg
        test/RecHitsToSeeds.cfg
        test/SeedToTrackCandidates.cfg
        test/SimG4Object.cfg
        test/SimHitsToDigis.cfg

--- src/SealModules.cc
+++ src/SealModules.cc
@@ -1,8 +1,9 @@
+
 #include "PluginManager/ModuleDef.h"
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "RecoTracker/CkfPattern/interface/CkfTrackCandidateMaker.h"
-using cms::CkfTrackCandidateMaker;
+#include "RecoTracker/CkfPattern/interface/KFTrackCandidateMaker.h"
+using cms::KFTrackCandidateMaker;
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(CkfTrackCandidateMaker)
+DEFINE_ANOTHER_FWK_MODULE(KFTrackCandidateMaker)
