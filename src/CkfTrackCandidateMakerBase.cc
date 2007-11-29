5adc22bc5c410396498aef711bc783a5150cec70
cmsbuild <>
2007-11-29 15:25:28 +0000

    This commit was manufactured by cvs2git to create tag 'RecoTracker- CkfPattern-V08-01-05'.
    Sprout from master 2007-11-29 15:25:24 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'Fixing trajectory filters for BHM, P5 and TIF'
    Cherrypick from master 2007-10-16 23:43:30 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'NavigationSchool from event setup':
        interface/CkfTrackCandidateMakerBase.h
        src/CkfTrackCandidateMakerBase.cc

--- src/CkfTrackCandidateMakerBase.cc
+++ src/CkfTrackCandidateMakerBase.cc
@@ -28,7 +28,8 @@
 #include "RecoTracker/CkfPattern/interface/SeedCleanerBySharedInput.h"
 #include "RecoTracker/CkfPattern/interface/CachingSeedCleanerBySharedInput.h"
 
-#include "RecoTracker/TkNavigation/interface/NavigationSchoolFactory.h"
+#include "RecoTracker/Record/interface/NavigationSchoolRecord.h"
+#include "TrackingTools/DetLayers/interface/NavigationSchool.h"
 
 #include<algorithm>
 #include<functional>
@@ -38,9 +39,8 @@ using namespace std;
 
 namespace cms{
   CkfTrackCandidateMakerBase::CkfTrackCandidateMakerBase(edm::ParameterSet const& conf) : 
-    conf_(conf),
-    theTrajectoryBuilderName(conf.getParameter<std::string>("TrajectoryBuilder")),
-    theTrajectoryCleaner(0),
+
+    conf_(conf),theTrajectoryBuilder(0),theTrajectoryCleaner(0),
     theInitialState(0),theNavigationSchool(0),theSeedCleaner(0)
   {  
     //produces<TrackCandidateCollection>();  
@@ -50,8 +50,6 @@ namespace cms{
   // Virtual destructor needed.
   CkfTrackCandidateMakerBase::~CkfTrackCandidateMakerBase() {
     delete theInitialState;  
-    delete theNavigationSchool;
-    delete theTrajectoryCleaner;    
     if (theSeedCleaner) delete theSeedCleaner;
   }  
 
@@ -64,17 +62,26 @@ namespace cms{
     // get nested parameter set for the TransientInitialStateEstimator
     ParameterSet tise_params = conf_.getParameter<ParameterSet>("TransientInitialStateEstimatorParameters") ;
     theInitialState          = new TransientInitialStateEstimator( es,tise_params);
-    theTrajectoryCleaner     = new TrajectoryCleanerBySharedHits();          
 
-    //theNavigationSchool      = new SimpleNavigationSchool(&(*theGeomSearchTracker),&(*theMagField));
-    edm::ParameterSet NavigationPSet = conf_.getParameter<edm::ParameterSet>("NavigationPSet");
-    std::string navigationSchoolName = NavigationPSet.getParameter<std::string>("ComponentName");
-    theNavigationSchool = NavigationSchoolFactory::get()->create( navigationSchoolName, &(*theGeomSearchTracker), &(*theMagField));
+    std::string trajectoryCleanerName = conf_.getParameter<std::string>("TrajectoryCleaner");
+    edm::ESHandle<TrajectoryCleaner> trajectoryCleanerH;
+    es.get<TrajectoryCleaner::Record>().get(trajectoryCleanerName, trajectoryCleanerH);
+    theTrajectoryCleaner= trajectoryCleanerH.product();
 
+    std::string navigationSchoolName = conf_.getParameter<std::string>("NavigationSchool");
+    edm::ESHandle<NavigationSchool> navigationSchoolH;
+    es.get<NavigationSchoolRecord>().get(navigationSchoolName, navigationSchoolH);
+    theNavigationSchool = navigationSchoolH.product();
 
     // set the correct navigation
     NavigationSetter setter( *theNavigationSchool);
 
+    // set the TrajectoryBuilder
+    std::string trajectoryBuilderName = conf_.getParameter<std::string>("TrajectoryBuilder");
+    edm::ESHandle<TrajectoryBuilder> theTrajectoryBuilderHandle;
+    es.get<CkfComponentsRecord>().get(trajectoryBuilderName,theTrajectoryBuilderHandle);
+    theTrajectoryBuilder = theTrajectoryBuilderHandle.product();    
+    
     std::string cleaner = conf_.getParameter<std::string>("RedundantSeedCleaner");
     if (cleaner == "SeedCleanerByHitPosition") {
         theSeedCleaner = new SeedCleanerByHitPosition();
@@ -96,11 +103,6 @@ namespace cms{
   { 
     // method for Debugging
     printHitsDebugger(e);
-
-    // get the TrajectoryBuilder out of the eventSetup
-    edm::ESHandle<TrajectoryBuilder> theTrajectoryBuilderHandle;
-    es.get<CkfComponentsRecord>().get(theTrajectoryBuilderName,theTrajectoryBuilderHandle);
-    const TrajectoryBuilder *theTrajectoryBuilder = theTrajectoryBuilderHandle.product();    
 
     // Step A: set Event for the TrajectoryBuilder
     theTrajectoryBuilder->setEvent(e);        
