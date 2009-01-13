849a85c6be15bdbe80fa8baea4a950388abc2f6b
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2009-01-13 04:34:27 +0000

    using grouped builder as in the standard tracking configuration

--- python/CkfTrackCandidatesPixelLess_cff.py
+++ python/CkfTrackCandidatesPixelLess_cff.py
@@ -2,14 +2,15 @@ import FWCore.ParameterSet.Config as cms
 
 # TrackerTrajectoryBuilders
 from RecoTracker.CkfPattern.CkfTrajectoryBuilderESProducer_cff import *
+from RecoTracker.CkfPattern.GroupedCkfTrajectoryBuilderESProducer_cff import *
 # TrajectoryCleaning
 from TrackingTools.TrajectoryCleaning.TrajectoryCleanerBySharedHits_cfi import *
 # navigation school
 from RecoTracker.TkNavigation.NavigationSchoolESProducer_cff import *
-import copy
-from RecoTracker.CkfPattern.CkfTrackCandidates_cfi import *
-# generate CTF track candidates ############
-ckfTrackCandidatesPixelLess = copy.deepcopy(ckfTrackCandidates)
-ckfTrackCandidatesPixelLess.TrajectoryBuilder = 'CkfTrajectoryBuilder'
-ckfTrackCandidatesPixelLess.SeedProducer = 'globalPixelLessSeeds'
 
+# generate CTF track candidates ############
+import RecoTracker.CkfPattern.CkfTrackCandidates_cfi 
+ckfTrackCandidatesPixelLess = RecoTracker.CkfPattern.CkfTrackCandidates_cfi.ckfTrackCandidates.clone(
+    TrajectoryBuilder = cms.string('GroupedCkfTrajectoryBuilder'),
+    SeedProducer = cms.string('globalPixelLessSeeds')
+    )
